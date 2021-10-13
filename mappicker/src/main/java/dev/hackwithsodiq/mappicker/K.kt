e8e752000e220fde803988c73dbb463666b33ac4
sodiqoladeni <Opeyemi-123!>
2021-10-13 19:26:19 +0100

    Fix gradle related issue 2

--- mappicker/src/main/java/dev/hackwithsodiq/mappicker/K.kt
+++ mappicker/src/main/java/dev/hackwithsodiq/mappicker/K.kt
@@ -2,5 +2,5 @@ package dev.hackwithsodiq.mappicker
 
 object K {
     const val LOCATION_DATA = "LOCATION_DATA"
-    const val LOCATION_ERROR = "val callerIntent = Intent()"
+    const val LOCATION_ERROR = "LOCATION_ERROR"
 }
